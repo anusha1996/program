a11a61e5859a5e1fc54d6e7ef215520f82130edc
anusha1996 <anushat199611@gmail.com>
2017-06-29 21:15:42 +0530

    Create posnegzero.java

---
+++ posnegzero.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class posnegzero
+{
+public static void main(String []s)
+{
+int num;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter any integer:");
+num=sc.nextInt()
+if(num>0)
+System.out.println(num + " is POSITIVE NUMBER.");
+else if(num<0)
+System.out.println(num + " is NEGATIVE NUMBER.");
+else
+System.out.println("IT's ZERO.")
+}
+}
